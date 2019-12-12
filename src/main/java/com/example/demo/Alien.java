bac47e051a2d381e2561e639bcb0170c77d3af4d
martmish <martmish@in.ibm.com>
2019-12-12 23:03:45 +0530

    Spring Rest data

---
+++ src/main/java/com/example/demo/Alien.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Alien {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+ private int id;
+ private String name;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+@Override
+public String toString() {
+	return "Alien [id=" + id + ", name=" + name + "]";
+}
+}
